a986288ebbde47fd40f50b17c577c6d4dd964f8f
AZIZXlaouiti <azizrocke@gmail.com>
2022-03-05 21:51:25 -0500

    testing css animation

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -4,7 +4,10 @@ consumer.subscriptions.create("RoomChannel", {
   connected() {
      console.log("connect...")
      $('#status').html("Waiting for another player")
-     
+     $('#turn').click(function(){
+       $('.player_hand').css({ transition: 'transform .2s ease,filter .5s ease',
+        transform: 'scale(0.8)'})
+     })
     // Called when the subscription is ready for use on the server
   },
 
@@ -18,3 +21,4 @@ consumer.subscriptions.create("RoomChannel", {
     // Called when there's incoming data on the websocket for this channel
   }
 });
+//<div class="player_hand"><img class="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Chkobba_carreau_01.svg/163px-Chkobba_carreau_01.svg.png" style="transform: rotate(-6deg);/* bottom: -2px; *//* z-index: 2; */left: 0px;/* opacity: 1; *//* filter: brightness(0.5); *//* pointer-events: none; */"><img class="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Chkobba_carreau_09.svg/163px-Chkobba_carreau_09.svg.png" style="transform: rotate(6deg);/* bottom: -12px; */z-index: 6;left: 219px;opacity: 1;filter: brightness(0.5);/* pointer-events: none; */"><img class="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Chkobba_pique_10.svg/163px-Chkobba_pique_10.svg.png" style="transform: rotate(0deg);bottom: -1px;z-index: 4;left: 100px;/* opacity: 1; */filter: brightness(0.5);/* pointer-events: none; */"></div>
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,28 +1,27 @@
 import React from "react"
 const App = () => {
   return (
-      <div className="game-field perspective red">
+    <div className="game-field perspective red">
 
-        <div id="player">
-          
-          <div id="tag">(You)</div>
+      <div id="player">
+
+        <div id="tag">(You)</div>
 
 
-
+      
           <div className="player_hand">
-            <div className="player_hand">
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Chkobba_carreau_01.svg/163px-Chkobba_carreau_01.svg.png"  />
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Chkobba_carreau_07.svg/163px-Chkobba_carreau_07.svg.png"  />
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Chkobba_pique_07.svg/163px-Chkobba_pique_07.svg.png"  />
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Chkobba_carreau_09.svg/163px-Chkobba_carreau_09.svg.png"  />
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Chkobba_pique_10.svg/163px-Chkobba_pique_10.svg.png"  />
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Chkobba_carreau_10.svg/163px-Chkobba_carreau_10.svg.png"  />
-              <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Chkobba_dos.svg/163px-Chkobba_dos.svg.png"  />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Chkobba_carreau_01.svg/163px-Chkobba_carreau_01.svg.png" />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Chkobba_carreau_07.svg/163px-Chkobba_carreau_07.svg.png" />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Chkobba_pique_07.svg/163px-Chkobba_pique_07.svg.png" />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Chkobba_carreau_09.svg/163px-Chkobba_carreau_09.svg.png" />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Chkobba_pique_10.svg/163px-Chkobba_pique_10.svg.png" />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Chkobba_carreau_10.svg/163px-Chkobba_carreau_10.svg.png" />
+            <img className="card" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Chkobba_dos.svg/163px-Chkobba_dos.svg.png" />
 
-            </div>
-          </div>
+         
         </div>
-    
+      </div>
+
       <div id="player_top">Top Player
         <div className="player_hand">
           <div className="card turned" data-index="0">
@@ -49,19 +48,23 @@ const App = () => {
         </div>
       </div>
       <div id="player_right">Right Player
-        
+
       </div>
       <div id="piles_area">
         <div id="draw_pile">
-          {/* <div className="card turned top-card">
-            <div className="bckg"></div>
-          </div> */}
-          
-        </div>
-        <div className="loading"></div>
-        
+          <button id="turn">turn</button>
+          <div className="stack"  id="1"></div>
+            <div className="stack"  id="2"></div>
+            <div id="3" className="stack" ></div>
+            <div id="4" className="stack" ></div>
+            <div id="5" className="stack" ></div>
+            <div id="6" className="stack" ></div>
+            <div id="7" className="stack" ></div>
+          </div>
+        <div className="direction.reverse"></div>
+
       </div>
-      </div>
+    </div>
   )
 }
 export default App
